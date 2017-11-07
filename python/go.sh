aceb52fce4fc83922a5c7a2bd44afffd9a14a087
Daniel Lawrence <dannyla@linux.com>
2017-11-07 00:57:46 -0800

    using debbie for fast packages

--- python/go.sh
+++ python/go.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker build -t build-python .
 DOCKER_ID=$(docker run -d build-python)
-docker cp ${DOCKER_ID}:/python36.tgz ~/Downloads/
+docker cp ${DOCKER_ID}:/tmp/python-3.6.1.all.deb ~/Downloads/
 docker stop ${DOCKER_ID}
 docker rm ${DOCKER_ID}
