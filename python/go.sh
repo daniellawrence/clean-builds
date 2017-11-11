fe9fd388db1d506b7d5b83c99fc8f78bdab30b8f
Daniel Lawrence <dannyla@linux.com>
2017-11-10 22:14:25 -0800

    working python36

--- python/go.sh
+++ python/go.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker build -t build-python .
 DOCKER_ID=$(docker run -d build-python)
-docker cp ${DOCKER_ID}:/tmp/python-3.6.1.all.deb ~/Downloads/
+docker cp ${DOCKER_ID}:/tmp/DSLpython36_3.6.3_all.deb ~/Downloads/
 docker stop ${DOCKER_ID}
 docker rm ${DOCKER_ID}
